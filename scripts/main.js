21e597aa7410bb8834aa8f34bfad153bce415782
JTBolger <112192375+JTBolger@users.noreply.github.com>
2024-02-27 21:32:38 -0600

    button pressing update
    now there are two functions in case the user didnt want to press the button

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,19 @@ const buttonFace = document.getElementById("button-face");
 
 buttonFace.addEventListener('mousedown', function() {
     buttonFace.classList.add('mouseDown');
+
+    buttonFace.addEventListener('mouseup', mouseUpHandler);
+    buttonFace.addEventListener('mouseleave', mouseLeaveHandler);
 });
 
-buttonFace.addEventListener('mouseup', function() {
+function mouseUpHandler() {
     buttonFace.classList.remove('mouseDown');
-});
+    buttonFace.removeEventListener('mouseup', mouseUpHandler);
+    buttonFace.removeEventListener('mouseleave', mouseLeaveHandler);
+}
+
+function mouseLeaveHandler() {
+    buttonFace.classList.remove('mouseDown');
+    buttonFace.removeEventListener('mouseup', mouseUpHandler);
+    buttonFace.removeEventListener('mouseleave', mouseLeaveHandler);
+}
